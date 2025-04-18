1216f0b92f30054edca0fd97de5bd0f9827e04a4
nivedr007 <nivedrndlap@gmail.com>
2025-04-18 23:37:41 +0530

    Modified tests

--- tests/test_user_logout.py
+++ tests/test_user_logout.py
@@ -1,50 +1,43 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 import pytest
 import time
-import json
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
-class TestLogout():
-  def setup_method(self, method):
-    self.driver = webdriver.Chrome()
-    self.vars = {}
+class TestLogout:
 
-  def teardown_method(self, method):
-    self.driver.quit()
+    def setup_method(self, method):
+        chrome_options = Options()
+        chrome_options.add_argument('--headless')  # Run in headless mode
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--disable-dev-shm-usage')
+        chrome_options.add_argument('--disable-gpu')
+        chrome_options.add_argument('--remote-debugging-port=9222')
 
-  def test_logout(self):
-    # Open the login page
-    self.driver.get("http://web:8000/login/")
-    self.driver.set_window_size(1057, 800)  # Set the window size
-    
-    # Enter username and password (hardcoded)
-    self.driver.find_element(By.NAME, "username").click()
-    self.driver.find_element(By.NAME, "username").send_keys("nived")
-    self.driver.find_element(By.NAME, "password").click()
-    self.driver.find_element(By.NAME, "password").send_keys("qwerty")
-    
-    # Click the login button
-    self.driver.find_element(By.CSS_SELECTOR, ".btn-dark").click()
+        # This assumes chromedriver is installed at /usr/local/bin/chromedriver
+        service = Service("/usr/local/bin/chromedriver")
 
-    # Click on the logout link
-    self.driver.find_element(By.LINK_TEXT, "Logout").click()
+        self.driver = webdriver.Chrome(service=service, options=chrome_options)
+        self.driver.get("http://web:8000/login")  # use container name in Docker network
 
-    # Wait for the logout modal to be visible and interactable
-    modal = WebDriverWait(self.driver, 10).until(
-        EC.visibility_of_element_located((By.ID, "logoutModal"))
-    )
-    
-    # Scroll to the logout button in case it is out of view
-    logout_button = modal.find_element(By.CSS_SELECTOR, ".btn-danger")
-    self.driver.execute_script("arguments[0].scrollIntoView(true);", logout_button)
+    def teardown_method(self, method):
+        self.driver.quit()
 
-    # Wait for the logout button to be clickable and then click it
-    logout_button = WebDriverWait(self.driver, 10).until(
-        EC.element_to_be_clickable((By.CSS_SELECTOR, ".btn-danger"))
-    )
-    logout_button.click()
+    def test_logout(self):
+        driver = self.driver
+
+        # Simulate login
+        driver.find_element(By.NAME, "username").send_keys("testuser")
+        driver.find_element(By.NAME, "password").send_keys("testpass")
+        driver.find_element(By.ID, "login-button").click()
+
+        time.sleep(2)
+
+        # Click logout
+        driver.find_element(By.ID, "logout-button").click()
+
+        time.sleep(2)
+
+        # Check if redirected to login page
+        assert "Login" in driver.title or "login" in driver.current_url
